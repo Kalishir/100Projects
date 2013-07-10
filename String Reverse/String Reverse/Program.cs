98f9923c9b4c0f2a0fa6e5b4b02fa58d19680ebf
David Birchall <kal_agrim@hotmail.co.uk>
2013-07-10 01:36:23 -0700

    Project 1 Completed
    String Reverse is Finished

---
+++ String Reverse/String Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace String_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the string to be reversed or enter E to exit:");
+                string input = Console.ReadLine();
+                if (input == "E" || input == "e")
+                    break;
+                else
+                {
+                    string output = "";
+                    for (int i = input.Length-1; i >=0; i--)
+                    {
+                        output += input[i];
+                    }
+                    Console.WriteLine("The reverse of " + input + " is " + output);
+                }
+            }
+        }
+    }
+}
